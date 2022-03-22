9816edb3e84f3079c577342a47d0ae38a92a0f91
kojirowilliam <kojirowilliamrocks@gmail.com>
2022-03-22 05:30:17 -0700

    Last Commit

--- src/Navbar/pages/memories.js
+++ src/Navbar/pages/memories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './memories.css';
+import collage from './photo-collage.png';
   
 const Memories = () => {
   return (
@@ -8,8 +9,11 @@ const Memories = () => {
         <h1>I was going to skimp out on this, but I felt bad, so....</h1>
         <h2 id="subheading">I made this:</h2>
       </div>
-      <div id="message">
-        <p1>Message...</p1>
+      <div>
+      <img src ={collage} alt="Picture of Morgan Collage" id="collage"/>
+      </div>
+      <div id="message1">
+        <p1>We've had a of good memories, most of which aren't recorded in photos or videos because I like to live in the moment. But the few that are, I sometimes look back at and remember how much our relationship means to me. The history that we've had together messing around Geffen or Kenneth Hahn is what makes our friendship so valuable and what's made my life worth living. So thank you for all that and the many more memories that are to come.</p1>
       </div>
       
     </div>

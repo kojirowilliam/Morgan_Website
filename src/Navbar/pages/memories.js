dcf9263af1a77483f02bce51c20d475a4ce305f7
kojirowilliam <kojirowilliamrocks@gmail.com>
2022-03-21 19:17:02 -0700

    seondar

--- src/Navbar/pages/memories.js
+++ src/Navbar/pages/memories.js
@@ -4,10 +4,10 @@ import './memories.css';
 const Memories = () => {
   return (
     <div class="wrapper">
-      <div id="header">
-        <h1>I was going to skimp out on this, but I felt bad.</h1>
+      <div id="header1">
+        <h1>I was going to skimp out on this, but I felt bad, so....</h1>
+        <h2 id="subheading">I made this:</h2>
       </div>
-      <br/>
       <div id="message">
         <p1>Message...</p1>
       </div>
